3d73ebdd5a3cbce989dd73ccaf31d3c0205c75ba
Stephen Brennan <smb196@case.edu>
2015-01-29 01:46:30 -0500

    Explanation for Challenge #7.

--- challenge07.py
+++ challenge07.py
@@ -3,6 +3,22 @@
 
 Start: http://www.pythonchallenge.com/pc/def/oxygen.html
 
+The strip of greyscale blocks in the image contains information.  Grey color has
+R=G=B, and the values are between 0 and 255.  Coincidentally, this can hold
+exactly one ASCII character of data.  The strip of grey in the image has 7 pixel
+wide blocks.  It starts at column 0 and ends at column 608.  It occurs around
+row 47.  Using that information, you can sample one pixel from each block, get
+the color value for each pixel, and get an ASCII character from that.  This
+allows you to build a string from the strip.
+
+However, that's not all.  The string has a complement, followed by a list of
+numbers, each of which code for a character in the final level name.  So, you
+need to convert those to a string, and then you have the name of the next level.
+
+This process could be done without pypng.  You'd need to use the color sampling
+tool on your image editor of choice.  But using a library like pypng allows me
+to automate it so that this solution runs just like any other one.
+
 """
 
 import webbrowser
